1a41007929e9f45765801aac9bc75a8719203f3f
Kevin Ndirangu <168788887+placcbo@users.noreply.github.com>
2026-01-14 13:49:15 +0000

    add:TestSafeDivide_ZeroNumerator

---
+++ week1/utils/safe_divide_test.go
@@ -0,0 +1,26 @@
+package utils
+
+import "testing"
+
+func TestSafeDivide_ValidDivision(t *testing.T) {
+	result, err := SafeDivide(10, 5)
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+	if result != 5 {
+		t.Errorf("expected 5. got %d", result)
+	}
+
+}
+
+func TestSafeDivide_ZeroNumerator(t *testing.T) {
+	result, err := SafeDivide(10, 0)
+
+	if err != nil {
+		t.Error("unexpected result %v", err)
+	}
+	if result != 0 {
+		t.Fatalf("expected 0, got %d", result)
+	}
+
+}
