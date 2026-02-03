da90536e0853ba89a32e514d03d25491a448f252
Kevin Ndirangu <168788887+placcbo@users.noreply.github.com>
2026-02-03 15:49:23 +0000

    feat: create transfer method

--- January/week3/Wallet.go
+++ January/week3/Wallet.go
@@ -20,8 +20,34 @@ func (w Wallet) Withdraw(amount int) (Wallet, bool) {
 	}
 	// Withdrawal possible, return new wallet
 	return Wallet{
-		Owner:   "kevin ndirangu",
+		Owner:   w.Owner,
 		Balance: w.Balance - amount,
 	}, true
 
+}
+
+func NewWallet(owner string, balance int) Wallet {
+	if balance < 0 {
+		balance = 0
+	}
+
+	return Wallet{
+		Owner:   owner,
+		Balance: balance,
+	}
+}
+
+func (w Wallet) Transfer(to Wallet, amount int) (Wallet, Wallet, bool) {
+	// Try to take money from sender (w)
+	newSender, ok := w.Withdraw(amount)
+	if !ok {
+		// Not enough money → return originals
+		return w, to, false
+	}
+
+	// Deposit into receiver (to)
+	newReceiver := to.Deposit(amount)
+
+	// Return updated wallets + success
+	return newSender, newReceiver, true
 }
